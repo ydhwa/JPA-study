35f4438495a57451e4afe33fbdfc465ffa8d5152
ydh <ydhwa_18@naver.com>
2019-09-04 13:50:53 +0900

    기본 세팅

---
+++ exercises/spring-jpa-best-practices/src/main/java/com/example/jpapractice/SpringJpaBestPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaBestPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaBestPracticesApplication.class, args);
+    }
+
+}
